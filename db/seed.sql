7afaf82f8af72b1ad767a678cc91b5f9f3dd580e
sujamanoj <suja@masstv.app>
2022-05-25 15:57:37 -0700

    update 3 with more employees

--- db/seed.sql
+++ db/seed.sql
@@ -4,5 +4,9 @@ VALUE ('vince','switzer','plant manager', 1, 1),
 ('mohammad','hussian','quality manager', 1, 1),
 ('nader','maddi','lead mechcanic', 2, 2),
 ('ricardo','bernal','supervisor', 3, 3),
-('petra','sermento','QA Lead', 2, 4);
+('petra','sermento','QA Lead', 2, 4),
+('stephen', 'stein', 'plant manager',1, 1),
+('alex','medena', 'process manager',1,5),
+('trishul','manoj','cfo',1, 1),
+('lynn','maggie','accounting',1,1);
 
