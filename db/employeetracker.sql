8a00344b06629298c1b4d20f19f0fd1f594b7cbc
sujamanoj <suja@masstv.app>
2022-05-25 18:45:40 -0700

    update 4 table companies

--- db/employeetracker.sql
+++ db/employeetracker.sql
@@ -1,19 +1,21 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS companies;
 
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY Key,
-    first_name VARCHAR(30) NOT null,
-    last_name VARCHAR(30) NOT null,
-    department VARCHAR(30) not null,
-    role_id INTEGER,
-    manager_id INTEGER
-);
 CREATE TABLE companies(
     id INTEGER AUTO_INCREMENT PRIMARY Key,
     name VARCHAR(50) NOT NULL,
     description TEXT
 );
-INSERT INTO companies(name, description)
-     VALUE ('Banza','dry pasta production'),
-     ('Caesars pasta','frozen foods'),
-     ('Mass tv','Multimedia production and steaming platform');
+
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT PRIMARY Key,
+    first_name VARCHAR(30) NOT null,
+    last_name VARCHAR(30) NOT null,
+    department VARCHAR(30) not null,
+    role_id INTEGER,
+    companies_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_companies FOREIGN Key (companies_id) REFERENCES companies(id) ON DELETE SET NULL
+);
+
 
--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,9 @@
+INSERT INTO companies(name, description)
+     VALUE ('Banza','dry pasta production'),
+     ('Caesars pasta','frozen foods'),
+     ('Mass tv','Multimedia production and steaming platform');
+
+
 INSERT INTO employee (first_name, last_name, department, role_id, manager_id)
 VALUE ('vince','switzer','plant manager', 1, 1),
 ('Gorge','cedano','process manager', 1, 1),
@@ -8,5 +14,9 @@ VALUE ('vince','switzer','plant manager', 1, 1),
 ('stephen', 'stein', 'plant manager',1, 1),
 ('alex','medena', 'process manager',1,5),
 ('trishul','manoj','cfo',1, 1),
-('lynn','maggie','accounting',1,1);
+('lynn','maggie','accounting',1,1),
+('suja','manoj','content manager',1,1),
+('vara','manoj','content supervisor', 3,6),
+('sana', 'manoj','marketing manager',1,6),
+('krishna', 'mahesh', 'content lead',2,6 );
 
